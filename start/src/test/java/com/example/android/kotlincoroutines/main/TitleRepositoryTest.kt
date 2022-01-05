7821a6fb2bc17086603ccda8c000934aa8387805
татьяна <emuart155@gmail.com>
2022-01-05 16:00:31 +0300

    Добавила тест для fun whenRefreshTitleSuccess_insertsRows()

--- start/src/test/java/com/example/android/kotlincoroutines/main/TitleRepositoryTest.kt
+++ start/src/test/java/com/example/android/kotlincoroutines/main/TitleRepositoryTest.kt
@@ -17,6 +17,12 @@
 package com.example.android.kotlincoroutines.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.kotlincoroutines.fakes.MainNetworkCompletableFake
+import com.example.android.kotlincoroutines.fakes.MainNetworkFake
+import com.example.android.kotlincoroutines.fakes.TitleDaoFake
+import com.google.common.truth.Truth
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
 
@@ -25,14 +31,48 @@ class TitleRepositoryTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    /**
+     * Средство выполнения тестов ничего не знает о сопрограммах,
+     * поэтому мы не можем сделать этот тест suspend функцией.
+     * В библиотеке kotlinx-coroutines-test есть runBlockingTest функция,
+     * которая блокирует, пока вызывается suspend функция.
+     *
+     * В этом тесте используются предоставленные подделки для проверки того,
+     * что "ОК" вставлено в базу данных с помощью refreshTitle.
+     * Когда тест вызывает runBlockingTest, он будет блокироваться до тех пор,
+     * пока не  завершится запуск сопрограммы .
+     * Затем внутри, когда мы вызываем, refreshTitle он использует обычный механизм
+     * приостановки и возобновления, чтобы дождаться добавления строки в нашу подделку базы данных.
+
+    После завершения тестовой сопрограммы runBlockingTest возвращается.
+     */
     @Test
-    fun whenRefreshTitleSuccess_insertsRows() {
-        // TODO: Write this test
+    fun whenRefreshTitleSuccess_insertsRows() = runBlockingTest {
+        val titleDao = TitleDaoFake("title")
+        val subject = TitleRepository(
+            MainNetworkFake("OK"),
+            titleDao
+        )
+
+        subject.refreshTitle()
+        Truth.assertThat(titleDao.nextInsertedOrNull()).isEqualTo("OK")
     }
 
+    /**
+     * Мы хотим добавить короткий таймаут к сетевому запросу.
+     */
     @Test(expected = TitleRefreshError::class)
-    fun whenRefreshTitleTimeout_throws() {
-        // TODO: Write this test
-        throw TitleRefreshError("Remove this – made test pass in starter code", null)
+    fun whenRefreshTitleTimeout_throws() = runBlockingTest {
+        val network = MainNetworkCompletableFake()
+        val subject = TitleRepository(
+            network,
+            TitleDaoFake("title")
+        )
+
+        launch {
+            subject.refreshTitle()
+        }
+
+        advanceTimeBy(5_000)
     }
 }
